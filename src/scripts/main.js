6feafefd5d6ba4b125a8614a8b5cdb31054d623a
Ovramenko Andrii <anovramenko2@gmail.com>
2025-03-01 18:27:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const list = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+
+function getEmployees(listOfPeople) {
+  const people = [];
+
+  for (const item of listOfPeople) {
+    people.push({
+      name: item.innerHTML.trim(),
+      age: item.dataset.age,
+      salary: item.dataset.salary,
+      position: item.dataset.position,
+    });
+  }
+
+  return people;
+}
+
+function sortList(listOfLi, listOfPeople) {
+  const newList = [...listOfPeople].sort((item1, item2) => {
+    return makeNumber(item2) - makeNumber(item1);
+  });
+
+  for (const item of newList) {
+    listOfLi.append(item);
+  }
+}
+sortList(ul, list);
+getEmployees(list);
+
+function makeNumber(str) {
+  return +str.dataset.salary.split(',').join('').slice(1);
+}
